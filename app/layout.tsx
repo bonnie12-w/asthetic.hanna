00abf772930d1b3ddbb14d64a69808c60cbf4ec9
bonnie12-w <bonfacekuggie@gmail.com>
2025-12-19 18:42:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css"
+import "./global.css"
 import { Playfair_Display, Inter } from "next/font/google"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"

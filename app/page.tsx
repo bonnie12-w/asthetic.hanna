69cdb6630e41477b51c12eccfb320b3d6c02832f
bonnie12-w <bonfacekuggie@gmail.com>
2025-12-29 07:45:20 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 50 }}>
+      <h1 style={{ fontSize: 48, color: "green" }}>
+        THIS IS DEFINITELY MY APP
+      </h1>
+    </div>
+  );
+}

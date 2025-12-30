2a6a5e58331824507c17ea532a3e8f1310085b28
bonnie12-w <bonfacekuggie@gmail.com>
2025-12-30 12:29:13 +0300

    Update CategoryCard.tsx

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link"
 import Image from "next/image"
+import Link from "next/link"
 
 interface CategoryCardProps {
   title: string
@@ -14,23 +14,21 @@ export default function CategoryCard({
 }: CategoryCardProps) {
   return (
     <Link href={href} className="block">
-      <div className="mx-auto max-w-[300px]">
-        <div className="relative aspect-[4/5] overflow-hidden">
-          <Image
-            src={imageUrl}
-            alt={title}
-            fill
-            sizes="300px"
-            className="object-cover"
-          />
+      <div className="relative aspect-[4/5] overflow-hidden bg-neutral-200">
+        <Image
+          src={imageUrl}
+          alt={title}
+          fill
+          className="object-cover"
+          priority
+        />
 
-          <div className="absolute inset-0 bg-gradient-to-t from-black/40 to-transparent" />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent" />
 
-          <div className="absolute bottom-0 left-0 p-4">
-            <h3 className="font-serif text-lg text-white">
-              {title}
-            </h3>
-          </div>
+        <div className="absolute bottom-0 left-0 p-4">
+          <h3 className="font-serif text-lg text-white">
+            {title}
+          </h3>
         </div>
       </div>
     </Link>
